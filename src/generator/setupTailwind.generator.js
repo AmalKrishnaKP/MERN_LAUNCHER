381f86ee560f0d97190b471dd7e32109c045e58c
AmalKrishnaKP <amalkrishnakp04@gmail.com>
2025-11-27 20:00:21 +0530

    tailwind setup completed

---
+++ src/generator/setupTailwind.generator.js
@@ -0,0 +1,45 @@
+import { execa } from "execa"
+import fs from "fs"
+import path from "path"
+
+export const setupTailwind=async(frontPath)=>{
+    try {
+        await execa("npm",["install", "tailwindcss", "@tailwindcss/vite"],{
+            cwd:frontPath,
+            stdio:"inherit"
+        })
+
+        const cssFile= path.join(frontPath,"src/index.css")
+        const updatedCssCont=`@import "tailwindcss";`
+        fs.writeFileSync(cssFile,updatedCssCont)
+
+        console.log("css updated");
+        
+
+
+        const htmlFile= path.join(frontPath,"index.html")
+        const htmlContent= fs.readFileSync(htmlFile,'utf8')
+        
+        const updatedHtmlCont= htmlContent.replace(/<\/title>\s*/i,`<title> \n \t <link href="/src/style.css" rel="stylesheet"> \n`)
+        console.log(updatedHtmlCont);
+        fs.writeFileSync(htmlFile,updatedHtmlCont)
+
+        console.log("html updated");
+        
+        
+        const configFile= path.join(frontPath,"vite.config.js")
+        const configCont= fs.readFileSync(configFile,'utf8')
+        const updatedConfigCont= configCont.replace(/plugins: \[\s*/,`plugins: [tailwindcss(),`)
+        fs.writeFileSync(configFile,`import tailwindcss from '@tailwindcss/vite' \n`+updatedConfigCont)
+        console.log(updatedConfigCont);
+
+        console.log("pluggin updated");
+        
+
+
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
--- src/setups/frontend.setups.js
+++ src/setups/frontend.setups.js
@@ -1,11 +1,12 @@
 import { execa } from "execa"
 import { setupVite } from "../generator/setupVite.generator.js";
+import { setupTailwind } from "../generator/setupTailwind.generator.js";
 
 export const frontendSetup=async(frontPath)=>{
     try {
 
         await setupVite(frontPath)
-
+        await setupTailwind(frontPath)
         
     } catch (error) {
         console.log(error);
